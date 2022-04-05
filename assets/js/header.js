cf7d899ab4832b86c7599aa26351c811724d8f77
Ivor <42016099+minecraftfen@users.noreply.github.com>
2022-04-05 14:12:44 +0800

    V2.0.1 ...

--- assets/js/header.js
+++ assets/js/header.js
@@ -3,5 +3,5 @@ body = document.body;
 // Scroll animation
 window.addEventListener('scroll', () => {
     var scrolled = document.scrollingElement.scrollTop;
-    body.style = "--scroll-ani:" + scrolled;
+    body.style = "--scroll-ani: " + scrolled + "px;";
 });
