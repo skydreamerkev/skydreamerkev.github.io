71d173e2847bf057151358d6e850259ff980c225
Ivor <42016099+minecraftfen@users.noreply.github.com>
2022-04-29 21:42:52 +0800

    New Icon!

---
+++ assets/js/libraries/console.js
@@ -0,0 +1,24 @@
+/* TODO: Override native methods
+    console.debug
+    console.log
+    console.info
+    console.trace
+    console.warn
+    console.error
+
+    behavior:
+        record the following information into `localStorage`
+        while completing the original function.
+            Log info
+            Timestamp
+            User’s ID
+            User Agent
+            IP Address
+            Device’s ID
+
+            Trace back
+                Code
+                File URL
+                File hash
+
+*/
